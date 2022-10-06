60d94babb3f6d11ccb3e899fdf6a824d72318577
Mac <mac@Mac.local>
2022-10-06 17:20:05 +0200

    add an authIsReady

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // styles
 import './App.css';
 // react router dom
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch,Redirect } from 'react-router-dom';
 // pages and component
 import Dashboard from './pages/dashboard/Dashboard';
 import Signup from './pages/signup/Signup';
@@ -10,37 +10,43 @@ import Create from './pages/create/Create';
 import Project from './pages/project/Project';
 import Navbar from './components/navabar/Navbar';
 import Sidebar from './components/sidebar/Sidebar';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const {user,authIsReady}= useAuthContext()
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Sidebar/>
-        <div className='container'>
-          <Navbar />
-          <Switch>
-            <Route exact path='/'>
-              <Dashboard />
-            </Route>
+      {/* when auth is ready and the user is ready  */}
+      {/* then the information will be shawn */}
+      {authIsReady && (
+        <BrowserRouter>
+          <Sidebar />
+          <div className='container'>
+            <Navbar />
+            <Switch>
+              <Route exact path='/'>
+                <Dashboard />
+              </Route>
 
-            <Route path='/create'>
-              <Create />
-            </Route>
+              <Route path='/create'>
+                <Create />
+              </Route>
 
-            <Route path='/projects/:id'>
-              <Project />
-            </Route>
+              <Route path='/projects/:id'>
+                <Project />
+              </Route>
 
-            <Route path='/login'>
-              <Login />
-            </Route>
+              <Route path='/login'>
+                <Login />
+              </Route>
 
-            <Route path='/signup'>
-              <Signup />
-            </Route>
-          </Switch>
-        </div>
-      </BrowserRouter>
+              <Route path='/signup'>
+                <Signup />
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
