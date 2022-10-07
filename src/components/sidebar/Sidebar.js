61dbe686822f4048d6fb79066a2c16b1bf7e2c45
Mac <mac@Mac.local>
2022-10-07 08:55:53 +0200

    add avatar comp to our sidebar to display user name and photo

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -4,14 +4,16 @@ import DashboardIcon from '../../assets/dashboard_icon.svg';
 import AddIcon from '../../assets/add_icon.svg';
 // Links
 import { NavLink } from 'react-router-dom';
-
+import Avatar from '../avatar/Avatar';
+import {useAuthContext} from '../../hooks/useAuthContext'
 export default function Sidebar() {
+  const {user} = useAuthContext()
   return (
     <div className='sidebar'>
       <div className='sidebar-content'>
         <div className='user'>
-          {/* avatar and username here later */}
-          <p>Hey user</p>
+          <Avatar src={user.photoURL} />
+          <p>Hey,<span>{user.displayName}</span></p>
         </div>
         <div className='links'>
           <ul>
@@ -27,7 +29,6 @@ export default function Sidebar() {
                 <span>New project</span>
               </NavLink>
             </li>
-            
           </ul>
         </div>
       </div>
