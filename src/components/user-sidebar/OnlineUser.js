ebcd52850f6e0bc292f4df3cac49ddc59e9e8cef
Mac <mac@Mac.local>
2022-10-07 09:37:02 +0200

    creating in online user comp a fetch user

--- src/components/user-sidebar/OnlineUser.js
+++ src/components/user-sidebar/OnlineUser.js
@@ -1,2 +1,21 @@
+import { useCollection } from '../../hooks/useCollection';
 // styles
-import './OnlineUser.css'
+import './OnlineUser.css';
+// component
+import Avatar from '../avatar/Avatar';
+export default function OnlineUser() {
+  const { error, documents } = useCollection('users');
+  return (
+    <div className='user-list'>
+      <h2>All users</h2>
+      {error && <div className='error'>{error}</div>}
+      {documents &&
+        documents.map((user) => (
+          <div key={user.id} className='user-list-item'>
+            <span>{user.displayName}</span>
+            <Avatar src={user.photoURL} />
+          </div>
+        ))}
+    </div>
+  );
+}
