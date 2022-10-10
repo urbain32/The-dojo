534e0b22deefcada61146140945859c14845e37d
Mac <mac@Mac.local>
2022-10-10 17:45:41 +0200

    Making a project list/Grid

--- src/components/projectList/ProjecList.js
+++ src/components/projectList/ProjecList.js
@@ -1,11 +1,26 @@
+import { Link } from 'react-router-dom';
 // styles
 import './ProjectList.css';
+// components
+import Avatar from '../avatar/Avatar'
 export default function ProjecList({ documents }) {
   return (
-    <div>
-      {documents.length == 0 && <p>No projects yet</p>}
+    <div className='project-list'>
+      {documents.length === 0 && <p>No projects yet</p>}
       {documents.map((doc) => (
-        <div key={doc.id}>{doc.name}</div>
+        <Link to={`/projects/${doc.id}`} key={doc.id}>
+          <h4>{doc.name}</h4>
+          <p>Due by {doc.dueDate.toDate().toDateString()}</p>
+          <div className='assigned-to'>
+            <ul>
+              {doc.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
