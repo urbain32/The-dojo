e48d03a627fb656ffe35ed5596277d8140043f7c
Mac <mac@Mac.local>
2022-10-11 10:04:38 +0200

    creating a project details(fetch project

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,7 +1,21 @@
 // styles
 import './Project.css'
+// other import
+import { useParams } from 'react-router-dom'
+import { useDocument } from '../../hooks/useDocument'
+import Loading from '../../components/loading/Loading'
 export default function Project() {
+  const { id } = useParams()
+  const { document, error } = useDocument('projects', id)
+  if (error) {
+    return <div className='error'>{error}</div>
+  }
+  if (!document) {
+    return <Loading/>
+  }
   return (
-    <div>Project Details</div>
-  )
+    <div className='project-details'>      
+      <h2>{document.name}</h2>
+    </div>
+  );
 }
