4a1c19a2b248919d6fcbd28eac7fb963fe6f5725
Mac <mac@Mac.local>
2022-10-11 16:55:55 +0200

    Listing added  comments

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { timestamp } from '../../firebase/config'
 import {useAuthContext} from '../../hooks/useAuthContext'
 import { useFirestore } from '../../hooks/useFirestore';
+import Avatar from '../../components/avatar/Avatar'
 export default function ProjectComments({project}) {
     const [newComment, setNewComment] = useState('')
     const { user } = useAuthContext()
@@ -26,6 +27,22 @@ export default function ProjectComments({project}) {
   return (
     <div className='project-comments'>
           <h4>Projects Comments</h4>
+          <ul>
+              {project.comments.length > 0 && project.comments.map(comment => (
+                  <li key={comment.id}>
+                      <div className="comment-author">
+                          <Avatar src={comment.photoURL} />
+                          <p>{comment.displayName}</p>
+                      </div>
+                      <div className="comment-date">
+                          <p>Date</p>
+                      </div>
+                      <div className="comment-content">
+                          <p>{comment.content}</p>
+                      </div>
+                  </li>
+              ))}
+          </ul>
           <form className="add-comment" onSubmit={handleSubmit}>
               <label >
                   <span>Add new comment</span>
