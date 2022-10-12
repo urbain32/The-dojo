33da4b8d1a8c876066a54750658ced470f30a20f
Mac <mac@Mac.local>
2022-10-12 08:34:08 +0200

    Removing auth stae console log

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -28,9 +28,7 @@ export const AuthContextProvider = ({ children }) => {
       unsub()
     })
   }, [])
-
-  console.log('AuthContext state:', state)
-  
+  // console.log('AuthContext state:', state)
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       { children }
