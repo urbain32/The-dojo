2fe3cb19bdbc194b99235c7fb4f67ed1e16c60e6
Mac <mac@Mac.local>
2022-10-13 12:07:04 +0200

    dashboard no changes

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,36 +4,38 @@ import { useCollection } from '../../hooks/useCollection';
 import './Dashboard.css';
 import ProjectFilter from './ProjectFilter';
 import { useState } from 'react';
-import {useAuthContext} from '../../hooks/useAuthContext'
+import { useAuthContext } from '../../hooks/useAuthContext';
 export default function Dashboard() {
-  const {user} = useAuthContext()
+  const { user } = useAuthContext();
   const { documents, error } = useCollection('projects');
   const [currentFilter, setCurrentFilter] = useState('all');
   const changeFilter = (newFilter) => {
     setCurrentFilter(newFilter);
   };
-  const projects = documents ? documents.filter(document => {
-    switch (currentFilter) {
-      case 'all':
-        return true;
-      case 'mine':
-        let assignedToMe = false;
-        document.assignedUsersList.forEach((loginUser) => {
-          if (user.uid === loginUser.id) {
-            assignedToMe = true;
-          }
-        });
-        return assignedToMe;
-      case 'development':
-      case 'design':
-      case 'sales':
-      case 'marketing':
-        console.log(document.category, currentFilter)
-        return document.category === currentFilter
-      default:
-        return true;
-    }
-  }):null
+  const projects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case 'all':
+            return true;
+          case 'mine':
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((loginUser) => {
+              if (user.uid === loginUser.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case 'development':
+          case 'design':
+          case 'sales':
+          case 'marketing':
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
   return (
     <div>
       <h2 className='page-title'>Dashboard</h2>
