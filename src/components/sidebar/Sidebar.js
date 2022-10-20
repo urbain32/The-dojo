88d4236d3b97ad4386791c307eb4b7d5b7c1972b
Mac <mac@Mac.local>
2022-10-20 17:33:14 +0200

    importing react icons

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,33 +5,34 @@ import AddIcon from '../../assets/add_icon.svg';
 // Links
 import { NavLink } from 'react-router-dom';
 import Avatar from '../avatar/Avatar';
-import {useAuthContext} from '../../hooks/useAuthContext'
+import {useAuthContext} from '../../hooks/useAuthContext';
+
 export default function Sidebar() {
-  const {user} = useAuthContext()
+  const {user} = useAuthContext();
   return (
-    <div className='sidebar'>
-      <div className='sidebar-content'>
-        <div className='user'>
-          <Avatar src={user.photoURL} />
-          <p>Hey,<span>{user.displayName}</span></p>
-        </div>
-        <div className='links'>
-          <ul>
-            <li>
-              <NavLink end to='/'>
-                <img src={DashboardIcon} alt=' dashboard icom' />
-                <span>Dashboard</span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/create'>
-                <img src={AddIcon} alt=' add projec icom' />
-                <span>New project</span>
-              </NavLink>
-            </li>
-          </ul>
+      <div className='sidebar'>
+        <div className='sidebar-content'>
+          <div className='user'>
+            <Avatar src={user.photoURL} />
+            <p>Hey,<span>{user.displayName}</span></p>
+          </div>
+          <div className='links'>
+            <ul>
+              <li>
+                <NavLink end to='/'>
+                  <img src={DashboardIcon} alt=' dashboard icom' />
+                  <span>Dashboard</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/create'>
+                  <img src={AddIcon} alt=' add projec icom' />
+                  <span>New project</span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
